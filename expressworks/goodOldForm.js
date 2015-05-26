8afcdf40bffafd1c901f77907d4edf774ffb10b2
clairtonluz <clairton.c.l@gmail.com>
2015-05-26 18:41:33 -0300

    good and old form

--- expressworks/goodOldForm.js
+++ expressworks/goodOldForm.js
@@ -3,12 +3,12 @@ var express = require('express')
 var app = express()
 var bodyparser = require('body-parser')
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyparser.urlencoded({extended: false})) //To parse x-www-form-urlencoded
 
 app.post('/form', function(req, res){
-  console.log('post')
+  console.log(req.body)
   res.end(req.body.str.split('').reverse().join(''))
-})
-
+});
 
 app.listen(process.argv[2] || 3000)
